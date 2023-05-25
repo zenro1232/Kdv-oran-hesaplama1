caa1ce66f0a0a746ddf1db3c914fc401f3547cec
zenro1232 <126719730+zenro1232@users.noreply.github.com>
2023-05-25 17:53:48 +0300

    Add files via upload

---
+++ src/kdv.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class kdv {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double  tutar,kdvoran = 0.18,Kdvtutar, Kdvlitutarı;
+        System.out.print("lütfen tutarı giriniz : " );
+
+
+
+        tutar = input.nextDouble();
+        Kdvtutar = tutar * kdvoran;
+        Kdvlitutarı = tutar + Kdvtutar;
+        System.out.println("Girdiginiz tutar : " + tutar);
+        System.out.println("kdv oranı : "+ 0.18);
+        System.out.println("Kdv tutarı :" + Kdvtutar);
+        System.out.println("Kdvli Tutar  :"+ Kdvlitutarı);
+
+
+    }
+}
